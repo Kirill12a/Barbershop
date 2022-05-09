581207a1cafb5b29d441cb208185e08436939f7e
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-05-09 17:31:37 +0300

    Initial Commit

---
+++ Barbershop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Barbershop
+//
+//  Created by Kirill Drozdov on 09.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
